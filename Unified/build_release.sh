04257ac6831ae24acbea81ae60efe1fa133b1405
Alexander Frolov <frolov.as@phystech.edu>
2026-05-20 16:20:52 +0300

    delete a bunch of stuff

--- Unified/build_release.sh
+++ Unified/build_release.sh
@@ -4,7 +4,7 @@ rm -r build
 mkdir build
 cd build
 
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx ..
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DDEBUG_SYMBOLS=1 ..
 #make -j 8  > >(tee build.log) 2> >(tee error.log >&2)
 ninja > >(tee build.log) 
 
